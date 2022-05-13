d1a1237616997d5322e96c0b6a48be91baf168eb
Piotr Zdolski <dante383@protonmail.com>
2022-05-13 20:50:01 +0200

    database seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'administrator',
+            'email' => 'test@example.com',
+            'password' => Hash::make('administrator1')
+        ]);
+    }
+}
