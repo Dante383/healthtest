e8d442740f6489e8fc23309619bf3d6d9076005b
Piotr Zdolski <dante383@protonmail.com>
2022-05-15 20:00:07 +0200

    GET practices api endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Practice;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/practices', function (Request $request) {
+    return Practice::all();
 });
